f44c53956110006e0915bcc64c11a991efca3a01
Govind Rajewar <abhirajewar87@gmail.com>
2024-08-03 15:56:45 +0530

    index.js: Removed commented code

--- index.js
+++ index.js
@@ -13,46 +13,6 @@ app.use(express.json());
 app.use(userRoutes);
 app.use(formDataRoutes);
 
-// Define the schema for form data
-// const itemSchema = new mongoose.Schema({
-//   id: String,
-//   type: String,
-//   src: String,
-//   placeholder: String,
-//   value: String,
-// });
-
-// const formSchema = new mongoose.Schema({
-//   formName: String,
-//   user: {
-//     userId: String,
-//     username: String,
-//     email: String,
-//   },
-//   itemList: [itemSchema],
-// });
-
-// const FormData = mongoose.model("FormData", formSchema);
-
-// app.post("/dynamic-items", async (req, res) => {
-//   try {
-//     console.log("Incoming request body:", req.body);
-
-//     // Validate request body
-//     if (!req.body.formName || !req.body.user || !req.body.itemList) {
-//       return res.status(400).send("Invalid data format");
-//     }
-
-//     // Save data to MongoDB
-//     const formData = new FormData(req.body);
-//     await formData.save();
-//     res.status(201).send(formData);
-//   } catch (error) {
-//     console.error("Error saving form data:", error);
-//     res.status(400).send(error.message);
-//   }
-// });
-
 app.get("/", (req, res) => {
   res.json({
     message: "Welcome to the FormBot Server",
