c668176adc6ddb19a0df2b676fb737d00b9b38cf
Govind Rajewar <abhirajewar87@gmail.com>
2024-10-24 18:36:32 +0530

    Date & Time fixed

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const dotenv = require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const userRoutes = require("./src/routes/User.js");
 const formDataRoutes = require("./src/routes/FormData.js");
+const port = process.env.PORT || 4000;
 
 const app = express();
 app.use(cors());
@@ -17,15 +18,38 @@ app.get("/", (req, res) => {
   res.json({
     message: "Welcome to the FormBot Server",
     Version: "0.2",
-    Date: `${new Date().getDate()}-${
-      new Date().getMonth() + 1
-    }-${new Date().getFullYear()}`,
+    Date: getData(),
+    Time: getTime(),
   });
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
   mongoose
     .connect(process.env.MONGODB_URL)
-    .then(() => console.log("Server is Connected to Database"))
+    .then(() => console.log(`Server is Connected to Database on ${port}`))
     .catch((err) => console.error("MongoDB connection error: ", err));
 });
+
+function getData() {
+  const day = new Date().getDate();
+  const month = new Date().getMonth() + 1;
+  const year = new Date().getFullYear();
+  return `${day < 10 ? `0${day}` : day}-${
+    month < 10 ? `0${month}` : month
+  }-${year}`;
+}
+
+function getTime() {
+  let hour = new Date().getHours();
+  const minute = new Date().getMinutes();
+  const second = new Date().getSeconds();
+  const period = hour >= 12 ? "PM" : "AM";
+
+  if (hour >= 12) {
+    hour = hour - 12;
+  }
+
+  return `${hour < 10 ? `0${hour}` : hour}:${
+    minute < 10 ? `0${minute}` : minute
+  }:${second < 10 ? `0${second}` : second} ${period}`;
+}
